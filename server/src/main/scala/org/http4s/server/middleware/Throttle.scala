5ae79880074ad8c08d600e0f272e2c6ebb6361f3
Keir Lawson <keirlawson@gmail.com>
2018-09-02 16:50:18 +0100

    Add Scaladoc

--- server/src/main/scala/org/http4s/server/middleware/Throttle.scala
+++ server/src/main/scala/org/http4s/server/middleware/Throttle.scala
@@ -12,21 +12,25 @@ sealed trait TokenAvailability
 case object TokenAvailable extends TokenAvailability
 case object TokenUnavailable extends TokenAvailability
 
-/*
- * TODO:
- * Function to decide on throttle response
- * Function to decide how many tokens
- * Make takeToken a function on a Request
- *
- * Consider replacing commented apply
- */
-
-//FIXME scaladoc
+/**
+  * A token bucket for use with the [[Throttle]] middleware.  Consumers can take tokens which will be refilled over time.
+  * Implementations are required to provide their own refill mechanism.
+  *
+  * Possible implementations include a remote TokenBucket service to coordinate between different application instances.
+  */
 trait TokenBucket[F[_]] {
   def takeToken: F[TokenAvailability]
 }
 
 object TokenBucket {
+
+  /**
+    * Creates an in-memory [[TokenBucket]].
+    *
+    * @param capacity the number of tokens the bucket can hold and starts with.
+    * @param refillEvery the frequency with which to add another token if there is capacity spare.
+    * @return A singleton stream containing the [[TokenBucket]].
+    */
   def local[F[_]](capacity: Int, refillEvery: FiniteDuration)(
       implicit F: Concurrent[F],
       timer: Timer[F]): Stream[F, TokenBucket[F]] =
@@ -48,13 +52,33 @@ object TokenBucket {
     }
 }
 
+/**
+  * Transform a service to reject any calls the go over a given rate with a 429 response.
+  */
 object Throttle {
-//  def apply[F[_], G[_]](amount: Int, per: FiniteDuration)(http: Http[F, G])(implicit F: Concurrent[F], timer: Timer[F]): Http[F, G] = {
-//    val refillFrequency = per / amount.toLong
-//    val createBucket = LocalTokenBucket(amount, refillFrequency)
-//    Kleisli.liftF(createBucket).flatMap(apply(_)(http))
-//  }
 
+  /**
+    * Limits the supplied service to a given rate of calls using an in-memory [[TokenBucket]]
+    *
+    * @param amount the number of calls to the service to permit within the given time period.
+    * @param per the time period over which a given number of calls is permitted.
+    * @param http the service to transform.
+    * @return a singleton stream containing the transformed service.
+    */
+  def apply[F[_], G[_]](amount: Int, per: FiniteDuration)(
+      http: Http[F, G])(implicit F: Concurrent[F], timer: Timer[F]): Stream[F, Http[F, G]] = {
+    val refillFrequency = per / amount.toLong
+    val createBucket = TokenBucket.local(amount, refillFrequency)
+    createBucket.map(bucket => apply(bucket)(http))
+  }
+
+  /**
+    * Limits the supplied service using a provided [[TokenBucket]]
+    *
+    * @param bucket a [[TokenBucket]] to use to track the rate of incoming requests.
+    * @param http the service to transform.
+    * @return a singleton stream containing the transformed service.
+    */
   def apply[F[_], G[_]](bucket: TokenBucket[F])(http: Http[F, G])(
       implicit F: Concurrent[F]): Http[F, G] =
     Kleisli { req =>
