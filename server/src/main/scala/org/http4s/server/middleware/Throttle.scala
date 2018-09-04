152f486152c33f28ece2a03e3750d965b8127daa
Keir Lawson <keirlawson@gmail.com>
2018-09-04 23:17:01 +0100

    Refill without background process, customisable response

--- server/src/main/scala/org/http4s/server/middleware/Throttle.scala
+++ server/src/main/scala/org/http4s/server/middleware/Throttle.scala
@@ -1,61 +1,83 @@
 package org.http4s.server.middleware
 
-import org.http4s.{Http, Response, Status}
+import org.http4s.{Headers, Http, Response, Status}
 import cats.data.Kleisli
-import cats.effect.{Concurrent, Timer}
+import cats.effect.{Clock, Sync}
 import cats.effect.concurrent.Ref
-import scala.concurrent.duration._
+import scala.concurrent.duration.FiniteDuration
 import cats.implicits._
-import fs2.Stream
-
-sealed trait TokenAvailability
-case object TokenAvailable extends TokenAvailability
-case object TokenUnavailable extends TokenAvailability
+import java.util.concurrent.TimeUnit.NANOSECONDS
+import org.http4s.headers.`Retry-After`
 
 /**
-  * A token bucket for use with the [[Throttle]] middleware.  Consumers can take tokens which will be refilled over time.
-  * Implementations are required to provide their own refill mechanism.
-  *
-  * Possible implementations include a remote TokenBucket service to coordinate between different application instances.
-  */
-trait TokenBucket[F[_]] {
-  def takeToken: F[TokenAvailability]
-}
-
-object TokenBucket {
-
-  /**
-    * Creates an in-memory [[TokenBucket]].
-    *
-    * @param capacity the number of tokens the bucket can hold and starts with.
-    * @param refillEvery the frequency with which to add another token if there is capacity spare.
-    * @return A singleton stream containing the [[TokenBucket]].
-    */
-  def local[F[_]](capacity: Int, refillEvery: FiniteDuration)(
-      implicit F: Concurrent[F],
-      timer: Timer[F]): Stream[F, TokenBucket[F]] =
-    Stream.eval(Ref[F].of(capacity)).flatMap { counter =>
-      val refill = Stream
-        .fixedRate[F](refillEvery)
-        .evalMap(_ =>
-          counter.update { count =>
-            Math.min(count + 1, capacity)
-        })
-      val bucket = new TokenBucket[F] {
-        override def takeToken: F[TokenAvailability] =
-          counter.modify({
-            case 0 => (0, TokenUnavailable)
-            case value: Int => (value - 1, TokenAvailable)
-          })
-      }
-      Stream(bucket).concurrently(refill)
-    }
-}
-
-/**
-  * Transform a service to reject any calls the go over a given rate with a 429 response.
+  * Transform a service to reject any calls the go over a given rate.
   */
 object Throttle {
+
+  sealed abstract class TokenAvailability extends Product with Serializable
+  case class TokenAvailable() extends TokenAvailability
+  case class TokenUnavailable(retryAfter: Option[FiniteDuration]) extends TokenAvailability
+
+  /**
+    * A token bucket for use with the [[Throttle]] middleware.  Consumers can take tokens which will be refilled over time.
+    * Implementations are required to provide their own refill mechanism.
+    *
+    * Possible implementations include a remote TokenBucket service to coordinate between different application instances.
+    */
+  trait TokenBucket[F[_]] {
+    def takeToken: F[TokenAvailability]
+  }
+
+  object TokenBucket {
+
+    /**
+      * Creates an in-memory [[TokenBucket]].
+      *
+      * @param capacity the number of tokens the bucket can hold and starts with.
+      * @param refillEvery the frequency with which to add another token if there is capacity spare.
+      * @return A task to create the [[TokenBucket]].
+      */
+    def local[F[_]](capacity: Int, refillEvery: FiniteDuration)(
+      implicit F: Sync[F],
+      clock: Clock[F]): F[TokenBucket[F]] = {
+
+      def getTime = clock.monotonic(NANOSECONDS)
+      val bucket = getTime.flatMap(time => Ref[F].of((capacity.toDouble, time)))
+
+      //FIXME calculate retry-after time - tests
+      bucket.map { counter =>
+        new TokenBucket[F] {
+          override def takeToken: F[TokenAvailability] = {
+            val attemptUpdate = counter.access.flatMap { case ((previousTokens, previousTime), setter) =>
+              getTime.flatMap( currentTime => {
+                val timeDifference = currentTime - previousTime
+                val tokensToAdd = timeDifference.toDouble / refillEvery.toNanos.toDouble
+                val newTokenTotal = Math.min(previousTokens + tokensToAdd, capacity.toDouble)
+
+                val attemptSet: F[Option[TokenAvailability]] = if (newTokenTotal >= 1) {
+                  setter((newTokenTotal - 1, currentTime)).map(success => if (success) Some(TokenAvailable()) else None)
+                } else {
+                  val timeToFull = (refillEvery.toNanos * capacity) - timeDifference
+                  val successResponse = TokenUnavailable(Some(FiniteDuration(timeToFull, NANOSECONDS)))
+                  setter((newTokenTotal, currentTime)).map(success => if (success) Some(successResponse) else None)
+                }
+
+                attemptSet
+              })
+            }
+
+            def loop: F[TokenAvailability] = attemptUpdate.flatMap { attempt =>
+              attempt.fold(loop)(token => token.pure[F])
+            }
+            loop
+          }
+
+        }
+      }
+
+    }
+
+  }
 
   /**
     * Limits the supplied service to a given rate of calls using an in-memory [[TokenBucket]]
@@ -63,28 +85,37 @@ object Throttle {
     * @param amount the number of calls to the service to permit within the given time period.
     * @param per the time period over which a given number of calls is permitted.
     * @param http the service to transform.
-    * @return a singleton stream containing the transformed service.
+    * @return a task containing the transformed service.
     */
   def apply[F[_], G[_]](amount: Int, per: FiniteDuration)(
-      http: Http[F, G])(implicit F: Concurrent[F], timer: Timer[F]): Stream[F, Http[F, G]] = {
+      http: Http[F, G])(implicit F: Sync[F], timer: Clock[F]): F[Http[F, G]] = {
     val refillFrequency = per / amount.toLong
     val createBucket = TokenBucket.local(amount, refillFrequency)
     createBucket.map(bucket => apply(bucket)(http))
+  }
+
+  def defaultResponse[F[_]](retryAfter: Option[FiniteDuration]): Response[F] = retryAfter match {
+    case Some(retryTime) => {
+      val retryHeader = `Retry-After`.unsafeFromDuration(retryTime)
+      Response[F](Status.TooManyRequests, headers = Headers(retryHeader))
+    }
+    case None => Response[F](Status.TooManyRequests)
   }
 
   /**
     * Limits the supplied service using a provided [[TokenBucket]]
     *
     * @param bucket a [[TokenBucket]] to use to track the rate of incoming requests.
+    * @param throttleResponse a function that defines the response when throttled, may be supplied a suggested retry time depending on bucket implementation.
     * @param http the service to transform.
-    * @return a singleton stream containing the transformed service.
+    * @return a task containing the transformed service.
     */
-  def apply[F[_], G[_]](bucket: TokenBucket[F])(http: Http[F, G])(
-      implicit F: Concurrent[F]): Http[F, G] =
+  def apply[F[_], G[_]](bucket: TokenBucket[F], throttleResponse: Option[FiniteDuration] => Response[G] = defaultResponse[G] _)(http: Http[F, G])(
+      implicit F: Sync[F]): Http[F, G] =
     Kleisli { req =>
       bucket.takeToken.flatMap {
-        case TokenAvailable => http(req)
-        case TokenUnavailable => Response[G](Status.TooManyRequests).pure[F]
+        case TokenAvailable() => http(req)
+        case TokenUnavailable(retryAfter) => throttleResponse(retryAfter).pure[F]
       }
     }
 }
--- server/src/test/scala/org/http4s/server/middleware/ThrottleSpec.scala
+++ server/src/test/scala/org/http4s/server/middleware/ThrottleSpec.scala
@@ -5,10 +5,12 @@ import cats.effect.laws.util.TestContext
 import org.http4s.{Http4sSpec, HttpApp, Request, Status}
 import cats.implicits._
 import org.http4s.dsl.io._
-import cats.effect.IO.ioConcurrentEffect
+import cats.effect.IO.ioEffect
+import org.http4s.headers.`Retry-After`
 import org.specs2.matcher.FutureMatchers
 import scala.concurrent.duration._
 import org.specs2.concurrent.ExecutionEnv
+import Throttle._
 
 class ThrottleSpec(implicit ee: ExecutionEnv) extends Http4sSpec with FutureMatchers {
   "LocalTokenBucket" should {
@@ -20,24 +22,20 @@ class ThrottleSpec(implicit ee: ExecutionEnv) extends Http4sSpec with FutureMatc
       val someRefillTime = 1234.milliseconds
       val capacity = 5
       val createBucket =
-        TokenBucket.local[IO](capacity, someRefillTime)(ioConcurrentEffect(testTimer), testTimer)
+        TokenBucket.local[IO](capacity, someRefillTime)(ioEffect, testTimer.clock)
 
-      val result = createBucket
-        .evalMap(testee => {
+      val takeExtraToken = createBucket
+        .flatMap(testee => {
           val takeFiveTokens: IO[List[TokenAvailability]] =
             (1 to 5).toList.traverse(_ => testee.takeToken)
           val checkTokensUpToCapacity =
-            takeFiveTokens.map(tokens => tokens must not contain TokenUnavailable)
+            takeFiveTokens.map(tokens => tokens must contain(TokenAvailable(): TokenAvailability).forall)
           checkTokensUpToCapacity *> testee.takeToken
         })
-        .compile
-        .toList
-        .map(_.head)
-        .unsafeToFuture()
 
-      ctx.tick()
+      val result = takeExtraToken.unsafeToFuture()
 
-      result must beEqualTo(TokenUnavailable).await
+      result must haveClass[TokenUnavailable].await
     }
 
     "add another token at specified interval when not at capacity" in {
@@ -46,17 +44,17 @@ class ThrottleSpec(implicit ee: ExecutionEnv) extends Http4sSpec with FutureMatc
 
       val capacity = 1
       val createBucket =
-        TokenBucket.local[IO](capacity, 100.milliseconds)(ioConcurrentEffect(testTimer), testTimer)
+        TokenBucket.local[IO](capacity, 100.milliseconds)(ioEffect, testTimer.clock)
 
-      val takeTokenAfterRefill = createBucket.evalMap(testee => {
+      val takeTokenAfterRefill = createBucket.flatMap(testee => {
         testee.takeToken *> testTimer.sleep(101.milliseconds) *> testee.takeToken
       })
 
-      val result = takeTokenAfterRefill.compile.toList.map(_.head).unsafeToFuture()
+      val result = takeTokenAfterRefill.unsafeToFuture()
 
       ctx.tick(101.milliseconds)
 
-      result must beEqualTo(TokenAvailable).await
+      result must beEqualTo(TokenAvailable()).await
     }
 
     "not add another token at specified interval when at capacity" in {
@@ -64,20 +62,52 @@ class ThrottleSpec(implicit ee: ExecutionEnv) extends Http4sSpec with FutureMatc
       val testTimer: Timer[IO] = ctx.timer[IO]
       val capacity = 5
       val createBucket =
-        TokenBucket.local[IO](capacity, 100.milliseconds)(ioConcurrentEffect(testTimer), testTimer)
+        TokenBucket.local[IO](capacity, 100.milliseconds)(ioEffect, testTimer.clock)
 
-      val takeExtraToken = createBucket.evalMap(testee => {
+      val takeExtraToken = createBucket.flatMap(testee => {
         val takeFiveTokens: IO[List[TokenAvailability]] = (1 to 5).toList.traverse(_ => {
           testee.takeToken
         })
         testTimer.sleep(300.milliseconds) >> takeFiveTokens >> testee.takeToken
       })
 
-      val result = takeExtraToken.compile.toList.map(_.head).unsafeToFuture()
+      val result = takeExtraToken.unsafeToFuture()
 
       ctx.tick(300.milliseconds)
 
-      result must beEqualTo(TokenUnavailable).await
+      result must haveClass[TokenUnavailable].await
+    }
+
+    "only return a single token when only one token available and there are multiple concurrent requests" in {
+      val ctx = TestContext()
+      val testTimer: Timer[IO] = ctx.timer[IO]
+      val capacity = 1
+      val createBucket =
+        TokenBucket.local[IO](capacity, 100.milliseconds)(ioEffect, testTimer.clock)
+
+      val takeTokensSimultaneously = createBucket.flatMap(testee => {
+        (1 to 5).toList.parTraverse(_ => testee.takeToken)
+      })
+
+      val result = takeTokensSimultaneously.unsafeToFuture()
+
+      result must contain(TokenAvailable() : TokenAvailability).exactly(1.times).await
+    }
+
+    "return the time until the bucket is refilled when no token is available" in {
+      val ctx = TestContext()
+      val testTimer: Timer[IO] = ctx.timer[IO]
+      val capacity = 1
+      val createBucket =
+        TokenBucket.local[IO](capacity, 1234.milliseconds)(ioEffect, testTimer.clock)
+
+      val takeTwoTokens = createBucket.flatMap(testee => {
+        testee.takeToken *> testee.takeToken
+      })
+
+      val result = takeTwoTokens.unsafeToFuture()
+
+      result must beEqualTo(TokenUnavailable(Some(1234.milliseconds))).await
     }
   }
 
@@ -88,7 +118,7 @@ class ThrottleSpec(implicit ee: ExecutionEnv) extends Http4sSpec with FutureMatc
 
     "allow a request to proceed when the rate limit has not been reached" in {
       val limitNotReachedBucket = new TokenBucket[IO] {
-        override def takeToken: IO[TokenAvailability] = TokenAvailable.pure[IO]
+        override def takeToken: IO[TokenAvailability] = TokenAvailable().pure[IO]
       }
 
       val testee = Throttle(limitNotReachedBucket)(alwaysOkApp)
@@ -99,13 +129,26 @@ class ThrottleSpec(implicit ee: ExecutionEnv) extends Http4sSpec with FutureMatc
 
     "deny a request when the rate limit had been reached" in {
       val limitReachedBucket = new TokenBucket[IO] {
-        override def takeToken: IO[TokenAvailability] = TokenUnavailable.pure[IO]
+        override def takeToken: IO[TokenAvailability] = TokenUnavailable(None).pure[IO]
       }
 
       val testee = Throttle(limitReachedBucket)(alwaysOkApp)
       val req = Request[IO](uri = uri("/"))
 
       testee(req) must returnStatus(Status.TooManyRequests)
+    }
+
+    "include a retry-after time in the response to a denied request when provided one by the token provider" in {
+      val someRetryTime = Some(1234.seconds)
+      val result = Throttle.defaultResponse(someRetryTime)
+
+      result.headers.get(`Retry-After`) must beSome(`Retry-After`.unsafeFromLong(1234))
+    }
+
+    "not include a retry-after time in the response to a denied request when the token provider does not supply one" in {
+      val result = Throttle.defaultResponse(None)
+
+      result.headers.get(`Retry-After`) must beNone
     }
   }
 }
